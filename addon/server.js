46a5b18993108ad3500f2e88d3b80cefbbd20410
tobi <msstl22977@gmail.com>
2026-09-13 20:34:58 +0500

    feat: ensure addon prioritizes local Wi-Fi LAN IP over VPN adapters

--- addon/server.js
+++ addon/server.js
@@ -20,7 +20,7 @@ function getLocalIp() {
   const nets = os.networkInterfaces();
   for (const name of Object.keys(nets)) {
     for (const net of nets[name] || []) {
-      if (net.family === "IPv4" && !net.internal) return net.address;
+      if (net.family === "IPv4" && !net.internal && (net.address.startsWith("192.168.") || name.toLowerCase().includes("wi-fi"))) return net.address;
     }
   }
   return "127.0.0.1";
